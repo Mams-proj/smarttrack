1a661e217a109c08502faed5263a54d50d35bdfc
Mams-proj <infinityzone16@gmail.com>
2026-04-01 17:50:20 +0300

    Update get_last_uid.php

--- api/get_last_uid.php
+++ api/get_last_uid.php
@@ -1,14 +1,10 @@
 <?php
-header('Content-Type: application/json');
-ini_set('display_errors', 0);
+require 'config.php';
 
-$file = "../data/last_uid.txt";
+$res = $conn->query("SELECT rfid FROM latest_scan WHERE id=1");
+$row = $res->fetch_assoc();
 
-if (!file_exists($file)) {
-    echo json_encode(["uid" => ""]);
-    exit;
-}
+$uid = $row['rfid'] ?? '';
 
-$uid = trim(file_get_contents($file));
 echo json_encode(["uid" => $uid]);
 ?>
